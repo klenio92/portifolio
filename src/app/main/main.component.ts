2ece8f4c9ab60a8ba289365dcb0aff15d9fd8a2e
Klenio <klenio92@gmail.com>
2024-02-20 12:01:27 -0300

    feat: add header, main and footer component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'portifolio-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+})
+export class MainComponent {}
