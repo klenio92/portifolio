ae1c4255bc15785fc50416fad41e8cc985964d65
Klenio <klenio92@gmail.com>
2024-02-28 15:18:53 -0300

    feat: add new routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, MainComponent, FooterComponent, RouterOutlet, RouterLink],
+  imports: [RouterModule, HeaderComponent, MainComponent, FooterComponent, ProjectsComponent,
+     AboutComponent, RouterOutlet, RouterLink],
   selector: 'portifolio-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Route } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const appRoutes: Route[] = [
     {
@@ -15,5 +17,13 @@ export const appRoutes: Route[] = [
     {
         path: '/footer',
         component: FooterComponent
+    },
+    {
+        path: '/about',
+        component: AboutComponent
+    },
+    {
+        path: '/projects',
+        component: ProjectsComponent
     },
 ];
