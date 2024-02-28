ed321681bd166f6b3312e7afd2672c57a6212016
Klenio <klenio92@gmail.com>
2024-02-28 15:17:47 -0300

    feat: add projects component

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'portifolio-projects',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css',
+})
+export class ProjectsComponent {}
