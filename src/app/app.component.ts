b0b597874e5f899fbf36710fb5194429e8965e2f
Klenio <klenio92@gmail.com>
2024-02-28 15:08:19 -0300

    add: routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, MainComponent, FooterComponent],
+  imports: [RouterModule, HeaderComponent, MainComponent, FooterComponent, RouterOutlet, RouterLink],
   selector: 'portifolio-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
