5be9bc768f7b15fc93d1444da2e30304835c1853
Klenio <klenio92@gmail.com>
2024-02-29 12:04:54 -0300

    feat: add tittles to each rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { ProjectsComponent } from './projects/projects.component';
 export const appRoutes: Route[] = [
     {
         path: 'about',
+        title: 'Sobre Klenio',
         component: AboutComponent
     },
     {
         path: 'projects',
+        title:'Meus Projetos',
         component: ProjectsComponent
     },
 ];
