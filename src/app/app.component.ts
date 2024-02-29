ceeb2acdb51873b78db765c65d6319eb8698be91
Klenio <klenio92@gmail.com>
2024-02-29 15:21:22 -0300

    feat: testing routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   standalone: true,
